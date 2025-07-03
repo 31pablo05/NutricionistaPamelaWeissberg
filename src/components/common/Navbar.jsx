b75f114bbbeff3745b24c1d24ece89ee8315d150
31pablo05 <Pabloproboste64@gmail.com>
2025-07-03 09:51:26 -0300

    ajuste nuevo a anvbar

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -41,6 +41,7 @@ const Navbar = () => {
               draggable="false"
               style={{ background: 'transparent', border: 'none' }}
             />
+
           </div>
           <span className="hidden md:block font-bold text-lg bg-gradient-to-r from-[#ee2b7a] via-[#fe9f5d] to-[#e1a1ed] bg-clip-text text-transparent tracking-tight drop-shadow-md transition-all duration-500 group-hover:drop-shadow-[0_0_8px_#ee2b7a80] group-focus:drop-shadow-[0_0_8px_#ee2b7a80] animate-gradient-x">
             Pamela Weissberg
